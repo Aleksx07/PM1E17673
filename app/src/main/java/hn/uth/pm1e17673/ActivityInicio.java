98cf0b1bc63363beb04776152a2494adc2ff03d4
Aleksx07 <134466359+Aleksx07@users.noreply.github.com>
2023-10-19 21:04:52 -0600

    Agregar pantallas

---
+++ app/src/main/java/hn/uth/pm1e17673/ActivityInicio.java
@@ -0,0 +1,14 @@
+package hn.uth.pm1e17673;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityInicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
